fdb9f8300fa06be85fdac8da3c9b0399e4b584eb
Ashok Mannem <ashokmannem01@gmail.com>
2021-02-23 23:55:43 +0000

    creating classes

---
+++ python_learning/creating_classes.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3.7
+class Car:
+    """
+    Car models a car w/ tires and an engine
+    """
+
+    def __init__(self, engine, tires):
+        self.engine = engine
+        self.tires = tires
+
+    def description(self):
+        print(f"A car with an {self.engine} engine, and {self.tires} tires")
