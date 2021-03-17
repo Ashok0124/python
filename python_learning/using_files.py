2452289074f70d72b78bc7665d5c63b5854a6220
Ashok Mannem <ashokmannem01@gmail.com>
2021-03-17 21:27:57 +0000

    using files

---
+++ python_learning/using_files.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3.7
+my_file = open('xmen.txt', 'w+')
+my_file.write('Beast\n')
+my_file.write('Phoenix\n')
+my_file.writelines([
+        'Cyclops\n',
+        'Bishop\n',
+        'Nightcrawler\n'
+        ])
+my_file.close()
+my_file = open('xmen.txt', 'r')
+print(my_file.read())
+print("I'm reading again")
+print(my_file.read())
