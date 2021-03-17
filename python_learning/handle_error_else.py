2b2454e87ae7b3320eb543b6bb6e581d043e6625
Ashok Mannem <ashokmannem01@gmail.com>
2021-03-17 22:08:40 +0000

    files and error handling

---
+++ python_learning/handle_error_else.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3.7
+
+file_name = 'recipes.txt'
+try:
+    my_file = open(file_name, 'x')
+    my_file.write(b'Meatballs\n')
+    my_file.close()
+except FileExistsError as err:
+    print(f"The {file_name} file already exists.")
+except:
+    print(f"Unable to write to the file")
+else:
+    print(f"Wrote to {file_name}")
+finally:
+    print("Execution completed")
+
