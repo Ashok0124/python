1debaddd8a5b07367fcd6a955c08d898c66cac77
Ashok Mannem <ashokmannem01@gmail.com>
2021-03-17 23:20:54 +0000

    python projects

---
+++ python_learning/projects/pgbackup/setup.py
@@ -0,0 +1,16 @@
+from setuptools import find_packages, setup
+
+with open('README.md', 'r') as f:
+    long_description = f.read()
+
+setup(
+        name='pgbackup'
+        version='0.1.0'
+        author='Ashok Mannem'
+        autho_email='test@email.com'
+        description='A utility for backing up PostgresSQL database'
+        long_description=long_description
+        long_description_content_type='text/markdown'
+        url='https://github.com/linuxacademy/pgbackup'
+        packages=find_packages('src')
+)
